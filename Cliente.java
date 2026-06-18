669ddda0fdfbfe39425420a88f75dddc8278edfd
Derick Catissi <153395984+derick-catissi@users.noreply.github.com>
2026-06-18 14:07:28 -0300

    Public class Cliente
    -Cadastro dos clientes

---
+++ Cliente.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Cliente {
+
+    private String nome;
+    private String telefone;
+    private List<Carro> veiculos;
+
+    public Cliente(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.veiculos = new ArrayList<>();
+    }
+
+    public void adicionarVeiculo(Carro carro) {
+        veiculos.add(carro);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public List<Carro> getVeiculos() {
+        return veiculos;
+    }
+}
