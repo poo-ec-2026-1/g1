17357f19ccd9217e582d49ffe603240211936c72
Derick Catissi <153395984+derick-catissi@users.noreply.github.com>
2026-06-18 13:55:47 -0300

    Classe CarroBateria
    -Public class
    -Extends Carro e implements IVerificavel

---
+++ CarroBateria.java
@@ -0,0 +1,21 @@
+public class CarroBateria extends Carro implements IVerificavel {
+
+    private int vidaUtilMeses;
+
+    public CarroBateria(String modelo, String placa, int vidaUtilMeses) {
+        super(modelo, placa);
+        this.vidaUtilMeses = vidaUtilMeses;
+    }
+
+    @Override
+    public String realizarCheckup() {
+        return "Checkup Elétrico [" + modelo + "]: Verificando voltagem e saúde da bateria.\n"
+             + "Vida útil estimada: " + vidaUtilMeses + " meses.";
+    }
+
+    @Override
+    public void apresentarProblema() {
+        System.out.println("Veículo elétrico " + modelo + " - Placa: " + placa
+                + " | Vida útil da bateria: " + vidaUtilMeses + " meses.");
+    }
+}
